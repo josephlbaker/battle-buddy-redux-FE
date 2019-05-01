abca6b08ac3b0133af5cd3229863d000c57d0694
josephlbaker <joelucasbaker@gmail.com>
2019-04-30 19:55:39 -0700

    testing login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { Grid, Segment, Button } from 'semantic-ui-react'
 
 export default class App extends Component {
+
+  state = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    username: "",
+    password: "",
+    isLoggedIn: "",
+    user: ""
+  }
 
   // fetchPosts = () => {
   //   fetch("http://localhost:3001/posts", {
@@ -15,11 +27,51 @@ export default class App extends Component {
   //   this.fetchPosts();
   // }
 
+  handleLogin = event => {
+    event.preventDefault();
+    axios
+      .post("http://https://battle-buddy-redux-be.herokuapp.com/users/login", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(res => {
+        let user = res.data.user
+        localStorage.token = res.data.signedJwt;
+        this.setState({
+          isLoggedIn: true,
+          redirect: true,
+          user
+        });
+        console.log(this.state.user);
+      })
+      .catch(err => console.log(err));
+  };
+
   render() {
     return (
-      <div>
-        Hello world
-      </div>
+      <form>
+        <Segment className="form-segment">
+          <Grid columns='equal'>
+            <Grid.Row>
+              <Grid.Column>
+                <input name="email" placeholder="Email" onChange={this.props.handleInput} />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <input type="password" name="password" placeholder="Password" onChange={this.props.handleInput} />
+              </Grid.Column>
+            </Grid.Row>
+            <Grid.Row>
+              <Grid.Column>
+                <Button fluid name="login" onClick={this.props.handleLogin} secondary>
+                  Submit
+                  </Button>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </form>
     )
   }
 }
