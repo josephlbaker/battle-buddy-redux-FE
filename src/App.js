b1260e94d169ae54ae0aed68aeb0e71ce3c30fd3
josephlbaker <joelucasbaker@gmail.com>
2019-05-01 13:52:56 -0700

    test

--- src/App.js
+++ src/App.js
@@ -26,39 +26,40 @@ export default class App extends Component {
   //   this.fetchPosts();
   // }
 
-  handleInput = event => {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  };
+  // handleInput = event => {
+  //   this.setState({
+  //     [event.target.name]: event.target.value,
+  //   });
+  // };
 
-  handleLogin = event => {
-    event.preventDefault();
-    axios
-      .post("https://battle-buddy-redux-be.herokuapp.com/users/login", {
-        email: this.state.email,
-        password: this.state.password
-      })
-      .then(res => {
-        let user = res.data.user
-        localStorage.token = res.data.signedJwt;
-        this.setState({
-          isLoggedIn: true,
-          redirect: true,
-          user
-        });
-        console.log(this.state.user);
-      })
-      .catch(err => console.log(err));
-  };
+  // handleLogin = event => {
+  //   event.preventDefault();
+  //   axios
+  //     .post("https://battle-buddy-redux-be.herokuapp.com/users/login", {
+  //       email: this.state.email,
+  //       password: this.state.password
+  //     })
+  //     .then(res => {
+  //       let user = res.data.user
+  //       localStorage.token = res.data.signedJwt;
+  //       this.setState({
+  //         isLoggedIn: true,
+  //         redirect: true,
+  //         user
+  //       });
+  //       console.log(this.state.user);
+  //     })
+  //     .catch(err => console.log(err));
+  // };
 
   render() {
     return (
-      <form>
-        <input name="email" placeholder="Email" onChange={this.handleInput} />
-        <input type="password" name="password" placeholder="Password" onChange={this.handleInput} />
-        <button name="login" onClick={this.handleLogin}>Submit</button>
-      </form>
+      // <form>
+      //   <input name="email" placeholder="Email" onChange={this.handleInput} />
+      //   <input type="password" name="password" placeholder="Password" onChange={this.handleInput} />
+      //   <button name="login" onClick={this.handleLogin}>Submit</button>
+      // </form>
+      <h1>Test</h1>
     )
   }
 }
